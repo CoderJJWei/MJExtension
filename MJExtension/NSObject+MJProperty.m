165b6f1e1a8feaafb0561f8f0b0fb90eb4bfc8eb
XMG <XMG@163.com>
2016-03-21 15:51:30 +0800

    修复了bug,重构了代码,大大优化了性能

--- MJExtension/NSObject+MJProperty.m
+++ MJExtension/NSObject+MJProperty.m
@@ -14,9 +14,7 @@
 #import "MJFoundation.h"
 #import <objc/runtime.h>
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wundeclared-selector"
-#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+
 
 static const char MJReplacedKeyFromPropertyNameKey = '\0';
 static const char MJReplacedKeyFromPropertyName121Key = '\0';
@@ -62,10 +60,7 @@ static NSMutableDictionary *cachedPropertiesDict_;
     if ([self respondsToSelector:@selector(mj_replacedKeyFromPropertyName121:)]) {
         key = [self mj_replacedKeyFromPropertyName121:propertyName];
     }
-    // 兼容旧版本
-    if ([self respondsToSelector:@selector(replacedKeyFromPropertyName121:)]) {
-        key = [self performSelector:@selector(replacedKeyFromPropertyName121) withObject:propertyName];
-    }
+
     
     // 调用block
     if (!key) {
@@ -82,10 +77,7 @@ static NSMutableDictionary *cachedPropertiesDict_;
     if (!key && [self respondsToSelector:@selector(mj_replacedKeyFromPropertyName)]) {
         key = [self mj_replacedKeyFromPropertyName][propertyName];
     }
-    // 兼容旧版本
-    if (!key && [self respondsToSelector:@selector(replacedKeyFromPropertyName)]) {
-        key = [self performSelector:@selector(replacedKeyFromPropertyName)][propertyName];
-    }
+    
     
     if (!key) {
         [self mj_enumerateAllClasses:^(__unsafe_unretained Class c, BOOL *stop) {
@@ -103,86 +95,8 @@ static NSMutableDictionary *cachedPropertiesDict_;
     return key;
 }
 
-+ (Class)propertyObjectClassInArray:(NSString *)propertyName
-{
-    __block id clazz = nil;
-    if ([self respondsToSelector:@selector(mj_objectClassInArray)]) {
-        clazz = [self mj_objectClassInArray][propertyName];
-    }
-    // 兼容旧版本
-    if ([self respondsToSelector:@selector(objectClassInArray)]) {
-        clazz = [self performSelector:@selector(objectClassInArray)][propertyName];
-    }
-    
-    if (!clazz) {
-        [self mj_enumerateAllClasses:^(__unsafe_unretained Class c, BOOL *stop) {
-            NSDictionary *dict = objc_getAssociatedObject(c, &MJObjectClassInArrayKey);
-            if (dict) {
-                clazz = dict[propertyName];
-            }
-            if (clazz) *stop = YES;
-        }];
-    }
-    
-    // 如果是NSString类型
-    if ([clazz isKindOfClass:[NSString class]]) {
-        clazz = NSClassFromString(clazz);
-    }
-    return clazz;
-}
 
-#pragma mark - --公共方法--
-+ (void)mj_enumerateProperties:(MJPropertiesEnumeration)enumeration
-{
-    // 获得成员变量
-    NSArray *cachedProperties = [self properties];
-    
-    // 遍历成员变量
-    BOOL stop = NO;
-    for (MJProperty *property in cachedProperties) {
-        enumeration(property, &stop);
-        if (stop) break;
-    }
-}
 
-#pragma mark - 公共方法
-+ (NSMutableArray *)properties
-{
-    NSMutableArray *cachedProperties = [self dictForKey:&MJCachedPropertiesKey][NSStringFromClass(self)];
-    
-    if (cachedProperties == nil) {
-        cachedProperties = [NSMutableArray array];
-        
-        [self mj_enumerateClasses:^(__unsafe_unretained Class c, BOOL *stop) {
-            // 1.获得所有的成员变量
-            unsigned int outCount = 0;
-            objc_property_t *properties = class_copyPropertyList(c, &outCount);
-            
-            // 2.遍历每一个成员变量
-            for (unsigned int i = 0; i<outCount; i++) {
-                MJProperty *property = [MJProperty cachedPropertyWithProperty:properties[i]];
-                // 过滤掉系统自动添加的元素
-                if ([property.name isEqualToString:@"hash"]
-                    || [property.name isEqualToString:@"superclass"]
-                    || [property.name isEqualToString:@"description"]
-                    || [property.name isEqualToString:@"debugDescription"]) {
-                    continue;
-                }
-                property.srcClass = c;
-                [property setOriginKey:[self propertyKey:property.name] forClass:self];
-                [property setObjectClassInArray:[self propertyObjectClassInArray:property.name] forClass:self];
-                [cachedProperties addObject:property];
-            }
-            
-            // 3.释放内存
-            free(properties);
-        }];
-        
-        [self dictForKey:&MJCachedPropertiesKey][NSStringFromClass(self)] = cachedProperties;
-    }
-    
-    return cachedProperties;
-}
 
 #pragma mark - 新值配置
 + (void)mj_setupNewValueFromOldValue:(MJNewValueFromOldValue)newValueFormOldValue
@@ -195,10 +109,7 @@ static NSMutableDictionary *cachedPropertiesDict_;
     if ([object respondsToSelector:@selector(mj_newValueFromOldValue:property:)]) {
         return [object mj_newValueFromOldValue:oldValue property:property];
     }
-    // 兼容旧版本
-    if ([self respondsToSelector:@selector(newValueFromOldValue:property:)]) {
-        return [self performSelector:@selector(newValueFromOldValue:property:)  withObject:oldValue  withObject:property];
-    }
+
     
     // 查看静态设置
     __block id newValue = oldValue;
